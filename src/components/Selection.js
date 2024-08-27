7aca87d8de64288b132d942f737bc95cf76134ff
rishika <rishikasoni2827@gmail.com>
2024-08-27 16:28:56 +0600

    Initial commit with React components

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,25 @@
+// src/components/Selection.js
+import React, { useState } from 'react';
+import '../styles/Child.css'; // Adjust this path as necessary
+
+
+const Selection = ({ applyColor }) => {
+  const [backgroundColor, setBackgroundColor] = useState('transparent');
+
+  const handleClick = () => {
+    applyColor((color) => {
+      setBackgroundColor(color.background);
+    });
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={{ backgroundColor: backgroundColor }}
+      onClick={handleClick}
+    />
+  );
+};
+
+export default Selection;
 
